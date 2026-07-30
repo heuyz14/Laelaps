7d101a56891e7ce89c7c2222ce6a6d71e6b0b3cb
heuyz14 <heuyz14@yahoo.com>
2026-07-30 15:43:27 +0700

    Ignore generated Next env types in lint

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [".next/**", ".next-build/**", "node_modules/**"],
+    ignores: [".next/**", ".next-build/**", "next-env.d.ts", "node_modules/**"],
   },
 ];
 
